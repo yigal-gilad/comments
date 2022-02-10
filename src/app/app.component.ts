1f796e7c78b85f01a4fa29132d6c026a7c10d00a
Yigal Gilad <yigaltheboss@gmail.com>
2022-02-10 16:58:34 +0200

    added delete function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,23 +18,23 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.oldLIst = this.comments.getJson();
-   this.updateView();
+    this.updateView();
   }
   title = 'comments';
 
-updateView() {
-  this.list = this.oldLIst;
-  localStorage.setItem("obj", JSON.stringify(this.oldLIst));
-  this.list.sort((a:any, b: any) => {
-    return <any>new Date(b.createdAt) - <any>new Date(a.createdAt);
-  });
-  this.list = this.list_to_tree(this.list);
+  updateView() {
+    this.list = this.oldLIst;
+    localStorage.setItem("obj", JSON.stringify(this.oldLIst));
+    this.list.sort((a: any, b: any) => {
+      return <any>new Date(b.createdAt) - <any>new Date(a.createdAt);
+    });
+    this.list = this.list_to_tree(this.list);
 
-  this.users = this.accounts.getJson();
-}
+    this.users = this.accounts.getJson();
+  }
 
   list_to_tree(list: any) {
-    var map: any = {}, node, roots = [], i;
+    var map: any = {}, node: any, roots = [], i;
 
     for (i = 0; i < list.length; i += 1) {
       map[list[i].id] = i; // initialize the map
@@ -43,7 +43,7 @@ updateView() {
 
     for (i = 0; i < list.length; i += 1) {
       node = list[i];
-      if (node.parentCommentId) {
+      if (node.parentCommentId && !node.deletedAt) {
         // if you have dangling branches check that map[node.parentId] exists
         list[map[node.parentCommentId]].children.push(node);
       } else {
@@ -55,24 +55,24 @@ updateView() {
 
   custom_sort([a, b]: any) {
     return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
-}
+  }
 
-  sortTree(tree: any){
-    tree.children.sort(function([a, b]: any){
-        
-        return new Date(b.createdAt)
+  sortTree(tree: any) {
+    tree.children.sort(function ([a, b]: any) {
+
+      return new Date(b.createdAt)
     });
     for (let i = 0; i < tree.children.length; i++) {
-        this.sortTree(tree.children[i])
+      this.sortTree(tree.children[i])
     }
-}
+  }
 
   findUserById = (id: string) => {
     let [key, user]: any = Object.entries(this.users).find(([key, user]: any) => user.id === id);
     return user;
   }
 
-  submit(text: string, parent? : number) {
+  submit(text: string, parent?: number) {
 
     this.oldLIst.push({
       "id": this.oldLIst.length,
@@ -86,13 +86,15 @@ updateView() {
     this.updateView();
     this.text = "";
   }
-  
-  updateOrDelete(id: number, text?: string) {
+
+  updateOrDelete(id: number, text?: string, obj?: any) {
     var index = this.oldLIst.findIndex((object: any) => {
       return object.id === id;
     });
     if (text) this.oldLIst[index].txt = text;
+    if (!text) this.oldLIst[index].deletedAt = new Date();
     this.text = "";
     this.updateView();
   }
+
 }
